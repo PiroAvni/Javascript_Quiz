8595fba175d702968b2f294ded7213b09307a2be
Avni Piro <90833537+PiroAvni@users.noreply.github.com>
2022-09-13 09:55:11 +0100

    javascript qiz

---
+++ app.js
@@ -0,0 +1,53 @@
+const correctAnswers =['A','A','A','A','A','B','A','C','C','B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+
+form.addEventListener('submit',e=>{
+    e.preventDefault();
+
+    let score = 0;
+    // takes the users answers can pushes them in to an array
+    const userAnswers =[
+        form.q1.value, 
+        form.q2.value,
+        form.q3.value,
+        form.q4.value,
+        form.q5.value,
+        form.q6.value,
+        form.q7.value,
+        form.q8.value,
+        form.q9.value,
+        form.q10.value,
+    ]
+    // check answers
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]){
+            score += 10;
+        }
+    });
+
+    // show result on page
+    scrollTo(0,0)
+    //result.querySelector('span').textContent =`${score}%`;
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const timer =setInterval(()=>{
+        result.querySelector('span').textContent =`${output}%`;
+        if(output === score){
+            clearInterval(timer)
+        }else{
+            output++;
+            console.log('Output;',output)
+        }
+        //console.log(output)
+    },10)
+    
+    console.log('The Correct Score',score)
+    console.log('The Correct Answers;',correctAnswers)
+    console.log('Users Answers:',userAnswers)
+    console.log(output)//0
+
+});
+// window object (global object)
